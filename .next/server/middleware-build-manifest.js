bcef9ff716841f77912aa98c87b6104056fd2ca3
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-28 00:03:36 -0400

    Fix deployment issues
    - Remove old src directory that was causing build failures
    - Update .gitignore to exclude .next directory and other build artifacts
    - Ensure clean build for production deployment

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/fd9d1056-729ec592c003a9f8.js","static/chunks/117-d485520dc6dc8208.js","static/chunks/main-app-8bf7da60edcc1676.js"],pages:{"/_app":["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-8ca55ea8b53394fb.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-8ca55ea8b53394fb.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
